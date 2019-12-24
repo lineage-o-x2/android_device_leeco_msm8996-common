8b87baf2c70d7780783183ecdb81d89c90397c6b
Marc Bourgoin <themard85@gmail.com>
2019-12-24 00:01:10 -0700

    msm8996-common: kang audio blobs from LA.UM.8.6.r1-01900-89xx.0
    * drop some QC audio effects and clean up audio effects while
      we're at it.
    * msm8996 uses 32bit-only audio
    
    Change-Id: If392d18ba774465d6f574b68b42548bf043e7b59

--- msm8996.mk
+++ msm8996.mk
@@ -88,8 +88,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@5.0-impl:32 \
+    android.hardware.audio.effect@5.0-impl:32 \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
